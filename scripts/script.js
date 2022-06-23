04045e31e82b97cf811db13143a914e6d2cc162b
pattah <rosiak211@gmail.com>
2022-06-23 20:31:11 +0200

    je

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,48 @@
-const add = (a, b) => a + b;
-const subtract = (a, b) => a - b;
-const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
+const add = (num1, num2) => num1 + num2;
+const subtract = (num1, num2) => num1 - num2;
+const multiply = (num1, num2) => num1 * num2;
+const divide = (num1, num2) => num1 / num2;
 
 
 const calcDisplay = document.querySelector('.output');
 
 calcDisplay.textContent = '0'
+let num1;
+let num2;
+let operator
+
+
+
+function operate () {
+  let result;
+if (operator === add) {
+   result = num1 + num2;
+} else if (operator === subtract) {
+  result = num1 - num2;
+} else if (operator === multiply) {
+  result = num1 * num2;
+} else if (operator === divide) {
+  result = num1 / num2;
+}
+return result
+}
+
+
 
 
 // Click events for every button on the calculator //
 function equals() {
 const buttonEquals = document.querySelector('.keyButtonEquals');
 buttonEquals.addEventListener('click', () => {
-  const buttons = document.querySelectorAll('.keyButton').forEach(button => {
-   const buttonContent = button.textContent;
-   const displayedNum = calcDisplay.textContent; 
+   if (num1 + num2) {
+     return operate ()
+   }
   
   });
    
-
-  
-  
-
-
-  });
 }
 
-
+equals();
 
 const buttonClear = document.querySelector('.keyButtonClear');
 buttonClear.addEventListener('click', () => {
@@ -44,24 +59,21 @@ buttonClear.addEventListener('click', () => {
 const buttons = document.querySelectorAll('.keyButton').forEach(button => {
   button.addEventListener('click', () => {
    removeClass();
-   
    const action = button.dataset.action;
-   const buttonContent = button.textContent;
+   num1 = button.textContent;
+   num2 = button.textContent;
    const displayedNum = calcDisplay.textContent;
    
    
-  
-   if (action) {
    if (displayedNum === "0") {
-     calcDisplay.textContent = buttonContent;
+     calcDisplay.textContent = num1;
     } else {
-      calcDisplay.textContent = displayedNum + buttonContent;  
+      calcDisplay.textContent = displayedNum + num1;  
    }
    
-  }
+  });
   
   });
-});
 function deleteInput() {
 const deleteButton = document.querySelector('.keyButtonDelete')
 deleteButton.addEventListener('click', () => {
@@ -70,32 +82,23 @@ deleteButton.addEventListener('click', () => {
 });
   
 }
-deleteInput()
+
 
 const buttonOperation = document.querySelectorAll('.keyButtonOperation').forEach(button => {
   button.addEventListener('click', () => {
     const action = button.dataset.action;
-    const buttonContent = button.textContent;
-   const displayedNum = calcDisplay.textContent;
-    
-   
+
   if (action === 'add' && add ||
   action === 'subtract' && subtract ||
   action === 'multiply' && multiply ||
   action === 'divide' && divide
 ) {
   button.classList.add('isDepressed')
-  
 }
-
-
-
-
-
   });
 
 });
-console.log (buttonOperation)
+
 
 function removeClass () {
   buttonRremove = document.querySelectorAll('.keyButtonOperation').forEach(button => {
